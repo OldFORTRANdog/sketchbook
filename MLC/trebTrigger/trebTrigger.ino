bb3936aa6ebf6804de800f03515437c786a15836
David L Eslinger <udave2007@gmail.com>
2015-01-19 22:57:57 -0500

    Reading a code successfully.  Still needs some debouncing work.

--- MLC/trebTrigger/trebTrigger.ino
+++ MLC/trebTrigger/trebTrigger.ino
@@ -9,7 +9,6 @@
  */
 #include <Wire.h>
 #include <Adafruit_MotorShield.h> 
-//#include <"utility/Adafruit_PWMServoDriver.h"
 #include <math.h>
 
 //#define BROWNFOB
@@ -36,8 +35,11 @@ unsigned long duration;
 byte direction;
 byte magnitude;
 
-boolean armed;
-boolean fired;
+boolean armed = false;
+boolean fired = true;
+
+String armCode = String("AADDC");
+String fireCode = String("ABC");
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
@@ -66,10 +68,6 @@ void setup(void){
      // set up Firing pin for testing
      pinMode(FIREPIN, OUTPUT);
      digitalWrite(FIREPIN, HIGH);
-     armed = false;
-     fired = false;
-     /* armCode = String("AADDC"); */
-     /* fireCode = String("ABC"); */
 }
 
 void loop(void) {
@@ -103,64 +101,97 @@ void loop(void) {
  /*  mTrigger->run(direction); */
  /*  delay(duration); */
 
-  //  if(codeEntered(armCode)) {
+  if(codeEntered(armCode)) {
     digitalWrite(FIREPIN,LOW);
     delay(1000);
     digitalWrite(FIREPIN,HIGH);
     delay(1000);
-    //  }
+  }
 }
 
-boolean codeEntered(secretCode) {
-  long timeOut = 5000; // Millsec between button pushes before code resets
-  long pressTime = millis();
+boolean codeEntered(String secretCode) {
+  unsigned long timeOut = 5000; // Millsec between button pushes before code resets
+  unsigned long pressTime = millis();
+  unsigned long debounce = 200;
   byte codePosition = 0;
-  char entry;
-
-  while (millis() - pressTime < timeOut) {
+  String entry;
+  byte oldFob = 0;
+  
+  while (millis() - pressTime < timeOut) { // Not timed out
     // Read FOB buttons using a binary incoding
-    byte fobValue = digitalRead(AFOBPIN) + // the 1's
-      2 * digitalRead(BFOBPIN) +           // the 2's
-      4 * digitalRead(CFOBPIN) +           // the 4's
-      8 * digitalRead(DFOBPIN);            // the 8's
-    
-    switch (fobValue) {
-    case(0):  // Nothing was pressed
-      entry = "";
-      break;
-    case(1):  // Button A was pressed
-      entry = "A";
-      break;
-    case(2):  // Button B was pressed
-      entry = "B";
-      break;
-    case(3):  // Button C was pressed
-      entry = "C";
-      break;
-    case(4):  // Button D was pressed
-      entry = "D";
-      break;
-    default:
-      entry = "";
-    }
-    Serial.print(entry);
-    Serial.println(secretCode);
-    if (entry != "") {       // there was a button pushed
-	if (secretCode[codePosition] = entry) { // Correct code entered
-	  codePosition++;
-	  if (codePosition == len(secretCode)) { // All code entered correctly
-	    return true;
-	  }
-	}
-	else { // incorrect code
-	  codePosition = 0;
-	  Serial.println("ERROR! Entered code incorrect. Code reset \g");
-	}
+    /* byte fobValue = digitalRead(AFOBPIN) + // the 1's */
+    /*   2 * digitalRead(BFOBPIN) +           // the 2's */
+    /*   4 * digitalRead(CFOBPIN) +           // the 4's */
+    /*   8 * digitalRead(DFOBPIN);            // the 8's */
+    byte fobValue = readFob();
+    if (fobValue > 0 &&  millis()-pressTime >=debounce)  { 
+      // there was a button pushed after the debounce period
       pressTime = millis();
-    }
+      switch (fobValue) {
+      case(1):  // Button A was pressed
+	entry = String('A');
+	break;
+      case(2):  // Button B was pressed
+	entry = String('B');
+	break;
+      case(4):  // Button C was pressed
+	entry = String('C');
+	break;
+      case(8):  // Button D was pressed
+	entry = String('D');
+	break;
+      default:
+	entry = String(' ');
+      }
+      Serial.print("entry = ");
+      Serial.print(entry);
+      Serial.print(", secretCode = ");
+      Serial.print(secretCode);
+      Serial.print(", fobValue = ");
+      Serial.print(fobValue);
+      Serial.print(", oldFob = ");
+      Serial.println(oldFob);
+
+      if (secretCode[codePosition] == entry[0]) { // Correct code entered
+	codePosition++;
+	if (codePosition == secretCode.length()) { // All code entered correctly
+	  return true;
+	}
+      }
+      else { // incorrect code
+	codePosition = 0;
+	Serial.println("ERROR! Entered code incorrect. Code reset \7");
+	return false;
+      }
+      pressTime = millis();
+      oldFob = fobValue;
+      // Delay for debounce of Fob keys
+    } // End of fobValue if block
   }
   codePosition = 0;  // Took too long
-  Serial.println("Timeout: Entered code reset \g");
+  Serial.println("Timeout: Entered code reset \7");
   return false;
 }
 
+
+byte readFob() {
+  /*  Reads fob button pushes, waits until nothing 
+      pushed to return a value  */
+  byte checkFob = 0;
+  byte buttonsUp = 15;
+  //  while (checkFob = 0) {
+    checkFob = digitalRead(AFOBPIN) + // the 1's
+      2 * digitalRead(BFOBPIN) + // the 2's
+      4 * digitalRead(CFOBPIN) + // the 4's
+      8 * digitalRead(DFOBPIN);  // the 8's
+    //  }
+  // Serial.println(checkFob);
+  /* while (buttonsUp > 0 ) { */
+  /*   buttonsUp = digitalRead(AFOBPIN) + // the 1's */
+  /*     2 * digitalRead(BFOBPIN) + // the 2's */
+  /*     4 * digitalRead(CFOBPIN) + // the 4's */
+  /*     8 * digitalRead(DFOBPIN);  // the 8's */
+  /*   Serial.println(buttonsUp); */
+  /* } */
+  return checkFob;
+}
